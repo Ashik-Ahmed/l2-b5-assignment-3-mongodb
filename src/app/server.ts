de3f68a8a4e496d147cdd5a910cfdb5eed423249
ashik-ahmed <ashikahmed121@gmail.com>
2025-06-20 21:11:19 +0600

    port initialization and mongodb cloud connected

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,12 +1,29 @@
 import { Server } from "http";
 import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import "colors";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 async function main() {
     try {
+
+        //database connection
+        const mongoUri = process.env.MONGODB_URI;
+
+        if (!mongoUri) {
+            throw new Error("MONGODB_URI environment variable is not defined");
+        }
+        mongoose.connect(mongoUri).then(() => {
+            console.log(`MongoDB Connection Successful...`.red.bold);
+        })
+
+        // Start the server
         const server: Server = app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+            console.log(`Server is running on port ${PORT}`.yellow.bold);
         });
     } catch (error) {
         console.error("Error starting the server:", error);
